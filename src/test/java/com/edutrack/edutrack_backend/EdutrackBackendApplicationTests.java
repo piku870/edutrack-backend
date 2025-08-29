ba6c906abe9e6844895389e0623e7c995b401f06
piku870 <pk0926228@gmail.com>
2025-08-29 12:07:43 +0530

    Initial Commit

---
+++ src/test/java/com/edutrack/edutrack_backend/EdutrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutrack.edutrack_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdutrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
